f3d1e442f935564d9b5a2072202461d5f7b90fc0
Reshad Farid <reshadfar@gmail.com>
2013-10-29 18:21:56 +0100

    modified textfield and JLabels for login form

--- src/utilities/LoginFormFields.java
+++ src/utilities/LoginFormFields.java
@@ -14,17 +14,30 @@ public class LoginFormFields extends JPanel {
 
     private CustomTextField username;
     private CustomPasswordField password;
+    private JLabel gebruikersnaam, wachtwoord;
 
     public LoginFormFields() {
 
         this.setBackground(Color.white);
+        this.setLayout(null);
 
         username = new CustomTextField(10);
-        password = new CustomPasswordField(10);
+        username.setBounds(35, 20, 205, 40);
 
-        add(new JLabel("Gebruikersnaam"));
+        password = new CustomPasswordField(10);
+        password.setBounds(35, 90, 205, 40);
+
+        gebruikersnaam = new JLabel("Gebruikersnaam");
+        gebruikersnaam.setBounds(35, -10, 205, 40);
+        gebruikersnaam.setFont(new Font("Tahoma", Font.PLAIN, 22));
+
+        wachtwoord = new JLabel("Wachtwoord");
+        wachtwoord.setBounds(35, 60, 205, 40);
+        wachtwoord.setFont(new Font("Tahoma", Font.PLAIN, 22));
+
+        add(gebruikersnaam);
         add(username);
-        add(new JLabel("Wachtwoord"));
+        add(wachtwoord);
         add(password);
 
 
