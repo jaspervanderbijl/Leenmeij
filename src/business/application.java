6d537ac1c633a6c6da98aaa4683ccd308984ccd6
Reshad Farid <reshadfar@gmail.com>
2013-10-29 15:05:26 +0100

    modified gitignore and added src file

---
+++ src/business/application.java
@@ -0,0 +1,11 @@
+package business;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: reshadfarid
+ * Date: 29-10-13
+ * Time: 15:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class application {
+}
